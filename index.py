6dd55d40de57def685fe973d91667317b4f7550b
Shamimgardobuya <shamimobuya@gmail.com>
2022-04-09 14:31:53 +0300

    made calculator

---
+++ index.py
@@ -0,0 +1,17 @@
+
+op=input("Enter operator type: ")  #make it below 1
+num1= int(input("Enter a number"))
+num2= int(input("Enter another number"))
+if op =="+":         
+    print(num1+num2)
+elif op=="*":
+    print(num1*num2)
+elif op =="/":
+    print(num1/num2)
+elif op =="-":
+    print(num1-num2)
+else :
+    print("Invalid operator")
+
+
+         
